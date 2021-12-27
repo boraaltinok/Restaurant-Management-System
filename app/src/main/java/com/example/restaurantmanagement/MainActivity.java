06797a7455c9054a0e39c48ed7d5a0dd66ad4323
boraaltinok <63509903+boraaltinok@users.noreply.github.com>
2021-12-27 18:12:51 +0300

    auth trial

--- app/src/main/java/com/example/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/example/restaurantmanagement/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.restaurantmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_main);
+
+        mAuth.createUserWithEmailAndPassword("admin@gmail.com", "pass");
+
+
     }
+
+
+
 }
